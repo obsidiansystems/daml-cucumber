4f865a1be90f9f8cc3fe6c10dc9076ebb2923e21
Ali Abrar <ali.abrar@obsidian.systems>
2024-01-12 18:39:04 -0400

    Fix build of daml-cucumber executable

--- hs/default.nix
+++ hs/default.nix
@@ -2,6 +2,7 @@
 let
   platform =  import ../nix/reflex-platform {};
   pkgs = platform.nixpkgs;
+  damlSdk = import ../nix/nix-daml-sdk {};
   src = builtins.filterSource (path: type: !(builtins.elem (baseNameOf path) [
       "release.nix"
       ".git"
@@ -17,9 +18,14 @@ let
       reflex-process = self.callCabal2nix "reflex-process" (pkgs.hackGet ./dep/reflex-process) {};
       which = haskellLib.doJailbreak super.which;
       reflex = self.callCabal2nix "reflex" (pkgs.hackGet ./dep/reflex) {};
+      daml-cucumber = haskellLib.overrideCabal
+        (self.callCabal2nix "daml-cucumber" src {})
+        (drv: {
+          librarySystemDepends = (drv.librarySystemDepends or []) ++ [ damlSdk.sdk ];
+        });
     };
   };
 in
-  { daml-cucumber = ghc.callCabal2nix "daml-cucumber" src {};
+  { daml-cucumber = ghc.daml-cucumber;
     inherit ghc;
   }
