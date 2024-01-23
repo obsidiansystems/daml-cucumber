5ddfdc4c878a3247f0d0cb9df50d3858577c7207
cidkidnix <cidkidnix@protonmail.com>
2024-01-23 15:51:06 -0600

    Build Container from multiple SDK versions

--- default.nix
+++ default.nix
@@ -1,25 +1,44 @@
 let
   platform = import ./nix/reflex-platform {};
   pkgs = platform.nixpkgs;
-  nix-daml-sdk = import ./nix/nix-daml-sdk {};
   hsBuild = import ./hs {};
-in pkgs.dockerTools.buildImage {
-  name = "daml-cucumber";
-  tag = "latest";
+  versions = builtins.map (x: let
+    version = builtins.replaceStrings [ ".json" ] [ "" ] x;
+    versionS = builtins.replaceStrings [ "." ] [ "" ] version;
+  in {
+    name = "daml-" + versionS;
+    value = rec {
+      recurseForDerivations = true;
+      damlSdk = import ./nix/nix-daml-sdk { sdkVersion = version; };
+      container = mkContainer damlSdk version;
+      inherit version;
+    };
+  })
+    (builtins.attrNames (builtins.readDir ((pkgs.hackGet ./nix/nix-daml-sdk) + "/versions")));
 
-  copyToRoot = pkgs.buildEnv {
-    name = "image-root";
-    paths = with nix-daml-sdk; [
-      sdk
-      pkgs.coreutils
-      (pkgs.haskell.lib.justStaticExecutables hsBuild.daml-cucumber)
-    ];
-    pathsToLink = [ "/bin" "/share" "/lib" ];
+  mkContainer = nix-daml-sdk: version: pkgs.dockerTools.buildImage {
+    name = "daml-cucumber";
+    tag = version;
+
+    copyToRoot = pkgs.buildEnv {
+      name = "image-root";
+      paths = with nix-daml-sdk; [
+        sdk
+        pkgs.coreutils
+        (pkgs.haskell.lib.justStaticExecutables hsBuild.daml-cucumber)
+      ];
+      pathsToLink = [ "/bin" "/share" "/lib" ];
+    };
+
+    config = {
+      Env = [
+        "PATH=/bin"
+      ];
+    };
   };
 
-  config = {
-    Env = [
-      "PATH=/bin"
-    ];
-  };
-}
+  outputs = builtins.listToAttrs versions;
+in {
+  container = outputs.daml-265.container;
+  recurseForDerivations = true;
+} // outputs
