f3cf4f9e2619aba77cb22b5dc61b20c5c2821e93
Luigy Leon <luigy.leon@obsidian.systems>
2024-01-25 13:00:00 -0500

    Ensure the same nix daml sdk is used

--- hs/default.nix
+++ hs/default.nix
@@ -3,7 +3,6 @@
 let
   platform =  import ../nix/reflex-platform {};
   pkgs = platform.nixpkgs;
-  damlSdk = nix-daml-sdk;
   src = builtins.filterSource (path: type: !(builtins.elem (baseNameOf path) [
       "release.nix"
       ".git"
@@ -23,12 +22,11 @@ let
       daml-cucumber = haskellLib.overrideCabal
         (self.callCabal2nix "daml-cucumber" src {})
         (drv: {
-          librarySystemDepends = (drv.librarySystemDepends or []) ++ [ damlSdk.sdk ];
+          librarySystemDepends = (drv.librarySystemDepends or []) ++ [ nix-daml-sdk.sdk ];
         });
     };
   };
 in
-  { daml-cucumber = ghc.daml-cucumber;
-    daml-sdk = damlSdk.sdk;
-    inherit ghc;
+  { inherit (ghc) daml-cucumber;
+    inherit ghc nix-daml-sdk;
   }
--- shell.nix
+++ shell.nix
@@ -2,18 +2,17 @@
 let
   platform =  import ./nix/reflex-platform {};
   pkgs = platform.nixpkgs;
-  nix-daml-sdk = import ./nix/nix-daml-sdk {};
+  hs = import ./hs {};
 in
   pkgs.mkShell {
     name = "daml-cucumber-test";
     buildInputs = [
       pkgs.cabal-install
       pkgs.ghcid
-      nix-daml-sdk.sdk
-      nix-daml-sdk.vscode
-      nix-daml-sdk.canton
+      hs.nix-daml-sdk.vscode
+      hs.nix-daml-sdk.canton
     ];
     inputsFrom = [
-      (import ./hs {}).daml-cucumber.env
+      hs.daml-cucumber.env
     ];
   }
