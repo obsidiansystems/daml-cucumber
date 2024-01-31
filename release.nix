c3bd50293e30b828af522745ca30345b2cf03548
cidkidnix <cidkidnix@protonmail.com>
2024-01-31 14:26:54 -0600

    Don't build containers in release.nix

--- release.nix
+++ release.nix
@@ -1,1 +1,8 @@
-import ./default.nix
+let
+  defNix = import ./default.nix;
+in builtins.mapAttrs (k: v: {
+  recurseForDerivations = true;
+  daml-cucumber = v.hsBuild.daml-cucumber;
+  daml-lib = v.damlLib;
+  daml-test = v.damlTest;
+  }) defNix.outputs
